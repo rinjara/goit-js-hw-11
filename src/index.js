2a42fec4fb57182e575eb3f1643e47722e1fd602
Krystyna Sharhorodska <rinjara@gmail.com>
2022-08-25 13:16:33 +0300

    correction axios

--- src/index.js
+++ src/index.js
@@ -4,6 +4,6 @@ import axios from 'axios';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
-const axios = require('axios').default;
+// const axios = require('axios').default;
 
 const ref = {};
