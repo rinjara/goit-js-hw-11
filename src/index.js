d9ef53829e43288538544c2f5098d0f54f6a615b
Krystyna Sharhorodska <rinjara@gmail.com>
2022-08-25 13:14:45 +0300

    adjustment of start files

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import { cardTemplate } from './js/templates';
+import axios from 'axios';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const axios = require('axios').default;
+
+const ref = {};
