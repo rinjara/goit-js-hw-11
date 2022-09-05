c7af9820a65df9da98c7259a8305768731b57053
Krystyna Sharhorodska <rinjara@gmail.com>
2022-09-05 13:04:55 +0300

    Looks like all done

--- src/index.js
+++ src/index.js
@@ -1,13 +1,8 @@
 import axios from 'axios';
-// import debounce from 'lodash.debounce';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 import { createGalleryCard } from './js/templates';
-// import { fetchData } from './js/fetchImages';
-
-// const axios = require('axios').default;
-// const DEBOUNCE_DELAY = 300;
 
 const KEY = `key=29520671-c96ba25b52e4cbbca2f07c463`;
 const SEARCH_PARAMS = 'image_type=photo&orientation=horizontal&safesearch=true';
@@ -19,7 +14,7 @@ const refs = {
   loadMoreBtn: document.querySelector('.load-more'),
 };
 
-const lightbox = new SimpleLightbox('.gallery a', {
+var lightbox = new SimpleLightbox('.gallery a', {
   captionsData: 'alt',
   captionPosition: 'bottom',
   captionDelay: 250,
@@ -30,39 +25,71 @@ axios.defaults.baseURL = `https://pixabay.com/api/`;
 let currentPage = 1;
 let query = '';
 let items = [];
-let totalPages = 0;
+let totalHits = 0;
+let hitsLeft = 0;
 
 const loadMoreOn = () => refs.loadMoreBtn.classList.remove('is-hidden');
 const loadMoreOff = () => refs.loadMoreBtn.classList.add('is-hidden');
 
-function fetchData(searchImg) {
-  axios
-    .get(
-      `?${KEY}&q=${searchImg}&${SEARCH_PARAMS}&per_page=${PER_PAGE}&page=${currentPage}`
-    )
-    .then(({ data }) => {
-      totalPages = data.totalHits / PER_PAGE;
-      console.log(items);
-      console.log(data);
-      console.log(totalPages);
-      items = data.hits;
-      render(items);
-    })
-    .catch(error => console.log(error.message));
+async function fetchData() {
+  try {
+    const { data } = await axios.get(
+      `?${KEY}&q=${query}&${SEARCH_PARAMS}&per_page=${PER_PAGE}&page=${currentPage}`
+    );
+
+    if (data.hits.length === 0) {
+      return Notify.failure(
+        'Sorry, there are no images matching your search query. Please try again.'
+      );
+    }
+
+    items = data.hits;
+    totalHits = data.totalHits;
+    hitsLeft = data.totalHits - data.hits.length;
+    render(items);
+    if (data.hits.length < PER_PAGE) {
+      Notify.info('We`re sorry, but you`ve reached the end of search results.');
+      loadMoreOff();
+    }
+  } catch (error) {
+    Notify.warning(error.message);
+    console.log(error.message);
+  }
 }
 
 function render() {
   const list = createGalleryCard(items);
-  refs.gallery.innerHTML = '';
   refs.gallery.insertAdjacentHTML('beforeend', list);
   loadMoreOn();
+  lightbox.refresh();
 }
 
-function handleSubmit(e) {
+async function handleSubmit(e) {
   e.preventDefault();
-  query = e.target.elements.searchQuery.value;
+  loadMoreOff();
+  refs.gallery.innerHTML = '';
+  currentPage = 1;
+
+  query = e.target.elements.searchQuery.value.trim();
+  if (!query) {
+    return;
+  }
   console.log(query);
-  fetchData(query);
+  await fetchData();
+  Notify.success(`Hooray! We found ${totalHits} images.`);
+}
+
+function onLoadMoreBtnClick() {
+  currentPage += 1;
+
+  if (hitsLeft === 0 || hitsLeft < 0) {
+    loadMoreOff();
+    return Notify.info(
+      'We`re sorry, but you`ve reached the end of search results.'
+    );
+  }
+  fetchData();
 }
 
 refs.searchForm.addEventListener('submit', handleSubmit);
+refs.loadMoreBtn.addEventListener('click', onLoadMoreBtnClick);
--- src/js/templates.js
+++ src/js/templates.js
@@ -9,29 +9,19 @@ export function createGalleryCard(images) {
         views,
         comments,
         downloads,
-      }) => `<a href="${largeImageURL} class="gallery__item">
-      <div class="photo-card">
-  <img src="${webformatURL}" alt="${tags}" loading="lazy" class="gallery__image"/>
-  <div class="info">
-    <p class="info-item">
-      <b>Likes</b>
-      ${likes}
-    </p>
-    <p class="info-item">
-      <b>Views</b>
-      ${views}
-    </p>
-    <p class="info-item">
-      <b>Comments</b>
-      ${comments}
-    </p>
-    <p class="info-item">
-      <b>Downloads</b>
-      ${downloads}
-    </p>
-  </div>
-  </div>
-</a>`
+      }) => `<a href="${largeImageURL}" class="photo-card">
+        <img src="${webformatURL}" alt="${tags}" loading="lazy" class="gallery__image"/>
+        <div class="info">
+        <p class="info-item">
+        <b>Likes</b>${likes}</p>
+        <p class="info-item">
+        <b>Views</b>${views}</p>
+        <p class="info-item">
+        <b>Comments</b>${comments}</p>
+        <p class="info-item">
+        <b>Downloads</b>${downloads}</p>
+        </div>
+        </a>`
     )
     .join('');
 }
